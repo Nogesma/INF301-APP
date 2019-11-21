5f70f489f1abea1fda294791d0d00de6a3d2ff48
Nogesma <mano.segransan@gmail.com>
2019-11-21 15:01:15 +0100

    Acte I-B

--- APP3/arbresphylo.c
+++ APP3/arbresphylo.c
@@ -1,34 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "arbres.h"
 #include "arbresphylo.h"
-#include "listes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int max(int a,int b){
-  if (a>=b)
+int max(int a, int b) {
+  if (a >= b)
     return a;
   else
     return b;
 }
 
-int hauteur (arbre racine){
-  if (racine ==NULL) //la racine est considérée de hauteur 1
+int hauteur(arbre racine) {
+  if (racine == NULL) // la racine est considérée de hauteur 1
     return 0;
   else
-    return 1+max(hauteur(racine.droit),hauteur(racine.gauche));
+    return 1 + max(hauteur(racine->droit), hauteur(racine->gauche));
 }
-
-
 
 /* ACTE II */
-/* Recherche l'espece dans l'arbre. Modifie la liste passée en paramètre pour y mettre les
- * caractéristiques. Retourne 0 si l'espèce a été retrouvée, 1 sinon.
+/* Recherche l'espece dans l'arbre. Modifie la liste passée en paramètre pour y
+ * mettre les caractéristiques. Retourne 0 si l'espèce a été retrouvée, 1 sinon.
  * Définissez un type de retour approprié !
  */
-int rechercher_espece (arbre racine, char *espece, liste_t* seq)
-{
-    /* à compléter */
-    return -1;
+int rechercher_espece(arbre racine, char *espece, liste_t *seq) {
+  /* à compléter */
+  return -1;
 }
-
--- APP3/listes.h
+++ APP3/listes.h
@@ -4,8 +4,4 @@
 typedef struct liste liste_t;
 /* Type de liste à compléter selon votre besoin. */
 
-
-
 #endif /* _LISTES_H */
-
-
