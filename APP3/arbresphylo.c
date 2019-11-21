46717425bfa53efd945a31f5a0cb6d7e28deed61
wwchesterww <wwchesterww@gmail.com>
2019-11-21 14:30:55 +0100

    Add compter to arbresphylo.c; Fix syntax problems

--- APP3/arbresphylo.c
+++ APP3/arbresphylo.c
@@ -16,10 +16,34 @@ int hauteur (arbre racine){
   if (racine ==NULL) //la racine est considérée de hauteur 1
     return 0;
   else
-    return 1+max(hauteur(racine.droit),hauteur(racine.gauche));
+    return 1+max(hauteur(racine->droit),hauteur(racine->gauche));
 }
 
+int estFeuille(arbre a){
+	return (a->gauche == NULL && a->droit == NULL);
+}
 
+void compter_rec(arbre a, int *nb_car, int *nb_esp){
+	if(a == NULL){exit(1);}
+	if(estFeuille(a)){
+		(*nb_esp) ++;
+	}
+	else{
+		(*nb_car) ++;
+		if(a->gauche != NULL){
+			compter_rec(a->gauche,nb_car,nb_esp);
+		}
+		if(a->droit != NULL){
+			compter_rec(a->droit,nb_car,nb_esp);
+		}
+	}
+}
+
+void compter(arbre a, int *nb_car, int *nb_esp){
+	*nb_car = 0;
+	*nb_esp = 0;
+	compter_rec(a,nb_car,nb_esp); 
+}
 
 /* ACTE II */
 /* Recherche l'espece dans l'arbre. Modifie la liste passée en paramètre pour y mettre les
